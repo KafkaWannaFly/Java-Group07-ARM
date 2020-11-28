b0d7e082cbdac40f52358ca79770907c687431dc
DuyDuong0602 <47823513+DuyDuong0602@users.noreply.github.com>
2020-11-28 16:30:24 +0700

    Update Items.java

--- ARM/src/Models/Items.java
+++ ARM/src/Models/Items.java
@@ -6,11 +6,17 @@ public class Items {
     private Long price;
     private String imgPath;
 
+    //CONSTRUCTOR
     public Items(){}
+    public Items(String type, String name, Long price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
 
     //SETTER
-    public void setName(){}
-    public void setPrice(){}
+    public void setName(String name){ this.name = name; }
+    public void setPrice(Long price){ this.price = price; }
     public void setImgPath(){}
 
     //GETTER
