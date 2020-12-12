3346788c20d1f72689274f5ec36bcb4fbf0a662c
DuyDuong0602 <47823513+DuyDuong0602@users.noreply.github.com>
2020-12-12 19:49:46 +0700

    Update create Bill function

--- ARM/src/Models/Bill.java
+++ ARM/src/Models/Bill.java
@@ -1,23 +1,52 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class Bill {
-	private HashMap<Items, Integer> dishes = new HashMap<>();
-	private String billID;
-	private Customer customer;
 
-	public Bill(){}
+	/* Để tránh việc đọc lại Menu database để lấy giá tiền từng món,
+	danh sách các món đã đặt sẽ gồm t
+	 */
+
+	private HashMap<String, Integer> dishesWithNumber = new HashMap<>();
+	private HashMap<String, Long> dishesWithPrice = new HashMap<>();
+	private String billID;
+	private String customerID;
+	private Long totalPrice;
+
+	public Bill(HashMap<String, Integer> dishesWithNumber,HashMap<String, Long> dishesWithPrice, Customer c) {
+
+		int price = 0;
+		Double VAT = 0.1;
+		/* Lấy thời gian hiện tại */
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		billID = dtf.format(now);
+
+		/* Kiểm tra thành viên */
+		if(c != null) {
+			customerID = c.getCustomerID();
+		}
+		else customerID = "-1";
+
+		for(String dishName : dishesWithNumber.keySet()) {
+			price += (dishesWithNumber.get(dishName) * dishesWithPrice.get(dishName));
+			this.dishesWithNumber.put(dishName, dishesWithNumber.get(dishName));
+			this.dishesWithPrice.put(dishName, dishesWithPrice.get(dishName));
+		}
+
+		totalPrice = price + Math.round(VAT*price);
+	}
 
 	//These two function will use on getBill()
-	private String getBillID(){
+	public String getBillID() {
 		return billID;
 	}
-	private Customer getCustomerInformation(){
-		return null;
-	}
 
-	public Bill MakeBill(){
-		return null;
+	public String getCustomerID() {
+		return this.customerID;
 	}
 }
